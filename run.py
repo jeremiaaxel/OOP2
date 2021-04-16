38de73097a4ccc8b1cc6f01522aaddc773598d2c
Jeremia Axel <jeremiaaxelbachtera@gmail.com>
2021-04-17 03:22:01 +0700

    Pindahin ke main src folder supaya bisa import

--- run.py
+++ run.py
@@ -18,49 +18,37 @@ from os.path import join
 
 current_dir = os.getcwd()
 src = "src"
-out = "out"
-main = "Main.java"
-
-battle_dir = join(src, "Battle")
-breeding_dir = join(src, "Breeding")
-engimon_dir = join(src, "Engimon")
-game_dir = join(src, "Game")
-map_dir = join(src, "Map")
-player_dir = join(src, "Player")
+main = join(src, "Main.java")
 
 def compile():
-    files = []
-    # files.extend(join(battle_dir, filename) for filename in os.listdir(battle_dir))
-    # files.extend(join(breeding_dir, filename) for filename in os.listdir(breeding_dir))
-    # files.extend(join(engimon_dir, filename) for filename in os.listdir(engimon_dir))
-    # files.extend(join(game_dir, filename) for filename in os.listdir(game_dir))
-    files.extend(join(map_dir, filename) for filename in os.listdir(map_dir))
-    # files.extend(join(player_dir, filename) for filename in os.listdir(player_dir))
+    print("Currently not working")
+    print("Try:")
+    print("1. cd src")
+    print("2. javac Main.java && java Main")
+    return
+    command = "javac " + main
 
-    for fil in files:
-        if fil.endswith(".class"):
-            files.remove(fil)
-
-    command = ""
-    for fil in files:
-        command += fil + " "
-
-    command = "javac -d " + out + " " + command
-
+    print(command)
     os.system(command)
     print("compiled")
 
-    os.system("cd " + out + " && java " + "Map")
+    os.system("java " + main)
 
 def clean():
-    for file in os.listdir(out):
-        os.remove(join(out, file))
+    for file in os.listdir(src):
+        if file.endswith(".class"):
+            os.remove(join(src, file))
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         compile()
     elif len(sys.argv) == 2 and sys.argv[1] == "clean":
         clean()
+    elif sys.argv[1] == "help":
+        print("Usage : <main> [args]")
+        print("args:")
+        print("  <none> : compile and run")
+        print("  clean  : clean the out directory")
     else:
         print("Usage : <main> [args]")
         print("args:")
